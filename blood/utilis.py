d7928405b31161b7511ab7a964b9f72cbc4ed6b3
guyst <stiggychinchilla@gmail.com>
2023-02-20 23:12:14 +0200

    Few changes

--- blood/utilis.py
+++ blood/utilis.py
@@ -1,16 +1,15 @@
 import requests
 
-email_user = 'guystephane78@gmail.com'
-password_user = 'Stephane2023@23'
+
 
 
 def send_sms(message, phone_number):
     data = {
         'recipients': f'{phone_number}',
         'message': f'{message}',
-        'sender': '0787313298',
+        'sender': '+250787313298',
     }
 
     r = requests.post('https://www.intouchsms.co.rw/api/sendsms/.json', data,
-                      auth=(f'guystephane78@gmail.com', f'Stephane2023@23'))
-    print(r.json(), r.status_code)
+                      auth=('Stephane', 'Stephane2023@23'), verify=False)
+    #print(r.json(), r.status_code)
