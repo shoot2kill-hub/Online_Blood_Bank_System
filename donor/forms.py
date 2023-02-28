9e859c7dc3fba64b39788359762f1c1344ab8262
guyst <stiggychinchilla@gmail.com>
2023-02-28 21:05:03 +0200

    Few changes

--- donor/forms.py
+++ donor/forms.py
@@ -19,4 +19,4 @@ class DonorForm(forms.ModelForm):
 class DonationForm(forms.ModelForm):
     class Meta:
         model=models.BloodDonate
-        fields=['age','bloodgroup','disease','unit']
+        fields=['pulse','temperature','bloodpressure','gender','occupation','weight','age','bloodgroup','disease','unit']
--- donor/models.py
+++ donor/models.py
@@ -22,9 +22,15 @@ class Donor(models.Model):
         return self.user.first_name
 
 class BloodDonate(models.Model): 
-    donor=models.ForeignKey(Donor,on_delete=models.CASCADE)   
+    donor=models.ForeignKey(Donor,on_delete=models.CASCADE)
     disease=models.CharField(max_length=100,default="Nothing")
     age=models.PositiveIntegerField()
+    gender=models.CharField(max_length=10)
+    occupation=models.CharField(max_length=20)
+    weight=models.CharField(max_length=10)
+    bloodpressure=models.CharField(max_length=10)
+    pulse=models.CharField(max_length=10)
+    temperature=models.CharField(max_length=10)
     bloodgroup=models.CharField(max_length=10)
     unit=models.PositiveIntegerField(default=0)
     status=models.CharField(max_length=20,default="Pending")
